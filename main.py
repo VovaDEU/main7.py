d7e459ee6241e96dbfba566ec1537e87895db4c8
VovaDEU <volodymyr.drobnyi@e-u.edu.ua>
2023-11-16 09:43:19 +0200

    Додано покращений код гри угадування числа

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import random
 import PySimpleGUI as sg
-target_number = random.randint(1, 100)
-def guess_number(values1,target_number):
-    values1 = int(values1)
 
+class NumberGuesser:
+    def __init__(self, target_number):
+        self.target_number = target_number
 
-    try:
+    def guess_number(self, user_guess):
+        try:
+            user_guess = int(user_guess)
 
+            if user_guess < self.target_number:
+                sg.popup("Загадане число більше.")
+            elif user_guess > self.target_number:
+                sg.popup("Загадане число менше.")
+            else:
+                sg.popup(f"Вітаємо! Ви вгадали число {self.target_number}")
+                return True  # Додали повернення True, щоб зупинити гру після вгадування
 
-        if values1< target_number:
-            print("Загадане число більше.")
-        elif values1> target_number:
-            print("Загадане число менше.")
-        else:
-            print(f"Вітаємо! Ви вгадали число {target_number}")
+        except ValueError:
+            sg.popup("Будь ласка, введіть ціле число.")
 
-    except ValueError:
-        print("Будь ласка, введіть ціле число.")
-sg.theme('DarkAmber')   # Add a touch of color
-layout = [  [sg.Text('Спробуйте вгадати число від 1 до 100:')],
-            [sg.Text('Вгадайте число'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-window = sg.Window('Window Title', layout)
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
+def main():
+    target_number = random.randrange(1, 101)
+    game = NumberGuesser(target_number)
+    max_attempts = 10  # Максимальна кількість спроб
+    attempt_count = 0
 
-    guess_number(values[0],target_number)
+    while attempt_count < max_attempts:
+        user_input = sg.popup_get_text("Спробуйте вгадати число від 1 до 100:")
 
+        if user_input is None:
+            break  # Користувач натиснув "Відміна" або закрив вікно
 
-window.close()
+        if game.guess_number(user_input):
+            break  # Якщо гравець вгадав, виходимо з гри
+
+        attempt_count += 1
+
+    if attempt_count == max_attempts:
+        sg.popup(f"Гру завершено. Було вичерпано {max_attempts} спроб. Загадане число було {target_number}.")
+
+if __name__ == "__main__":
+    main()
